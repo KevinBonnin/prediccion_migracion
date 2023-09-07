2e1d5df856d808374d06def0d570da9fe0c2ec62
KevinBonnin <kevin.bonnin65@gmail.com>
2023-09-07 02:05:06 -0300

    1

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from fastapi import FastAPI
+import uvicorn
+import joblib
+import mysql.connector
+
+conexion = mysql.connector.connect(user ='admin', password = 'migration2023', host = 'database-migration.cq1xp27nrjmz.us-east-2.rds.amazonaws.com', database = 'migration', port = '3306')
+
+indicadores = pd.read_sql("SELECT * FROM migration.indicadores", conexion)
+
+def obtener_predicciones_por_pais(df, modelo):
+    predicciones_por_pais = pd.DataFrame()
+    for index, fila in df.iterrows():
+        variables_independientes = fila[["Crecimiento_PIB", "Tasa_desempleo", "Inflacion_PIB", "Control_Corrupcion", "Emisiones_CO2","Esperanza_vida"]].values.reshape(1, -1)
+        prediccion = modelo.predict(variables_independientes)[0]
+        pais = fila["Migracion_neta"]
+        predicciones_por_pais = predicciones_por_pais.append({"País": pais, "Predicción": prediccion}, ignore_index=True)
+    return predicciones_por_pais
+
+modelo = joblib.load('modelo_regresion.pkl')
+
+app = FastAPI()
+
+@app.get("/modelo de prediccion")
+def prediccion():
+    predicciones = obtener_predicciones_por_pais(indicadores, modelo)
+    return predicciones
