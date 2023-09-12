868b428da3fc2fbd56c37304f73f58887a5ab67c
KevinBonnin <kevin.bonnin65@gmail.com>
2023-09-11 23:39:55 -0300

    final

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ modelo = joblib.load('modelo_migracion.pkl')
 
 app = FastAPI()
 
+#class Pais(Enum):
+ #   Argentina = [5, 6, 8, 5, 7, 4]
+  #  Brasil = [7, 8, 6, 2, 1, 5]
+   # Chile = [5.2, 8.3, 2.1, 100, 75, 78.5]
+    
 class Pais(Enum):
-    Argentina = [5, 6, 8, 5, 7, 4]
-    Brasil = [7, 8, 6, 2, 1, 5]
-    Chile = [5.2, 8.3, 2.1, 100, 75, 78.5]
+    Argentina = (5, 6, 8, 5, 7, 4)
+    Brasil = (7, 8, 6, 2, 1, 5)
+    Chile = (5.2, 8.3, 2.1, 100, 75, 78.5)
 
 @app.get("/Modelo de prediccion")
 def prediccion_flujo(crecimiento_pib,tasa_desempleo,inflacion_pib,muertes_conflicto,control_corrupcion,esperanza_vida):
@@ -27,6 +32,13 @@ def prediccion_flujo(crecimiento_pib,tasa_desempleo,inflacion_pib,muertes_confli
 def prediccion_flujo1(pais: Pais):
     datos_prediccion = {}
     datos_prediccion = [[pais[0], pais[1], pais[2], pais[3], pais[4], pais[5]]]
+    df = pd.DataFrame(datos_prediccion)
+    prediccion = modelo.predict(df)
+    return prediccion[0]
+
+@app.get("/modelo3")
+def prediccion_flujo1(pais: Pais):
+    datos_prediccion = [[*pais.value]]  # Convierte los valores del Enum en una lista
     df = pd.DataFrame(datos_prediccion)
     prediccion = modelo.predict(df)
     return prediccion[0]
